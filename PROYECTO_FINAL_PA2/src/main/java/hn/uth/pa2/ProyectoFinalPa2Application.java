9a7ee8fe5a0803ca2aa2405ee7cbcf7912cc285c
Brayan Alvarez <67676953+BrayanUTH@users.noreply.github.com>
2021-03-30 20:56:20 -0600

    Add files via upload

---
+++ PROYECTO_FINAL_PA2/src/main/java/hn/uth/pa2/ProyectoFinalPa2Application.java
@@ -0,0 +1,13 @@
+package hn.uth.pa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPa2Application.class, args);
+	}
+
+}
---
+++ PROYECTO_FINAL_PA2/src/test/java/hn/uth/pa2/ProyectoFinalPa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.uth.pa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
